23ef398a13246c5049729eabb54155403a5e0173
raghav <raghavdesai774@gmail.com>
2025-11-09 21:28:37 +0530

    developer's image changed

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ STARFIELD (Canvas Parallax)
 const canvas = document.getElementById('star-canvas');
 const ctx = canvas.getContext('2d');
 let w, h, dpr; let stars = [];
+// faster
 // const LAYERS = [
 //     { depth: 0.25, count: 120, speed: 0.02 },
 //     { depth: 0.6, count: 180, speed: 0.06 },
 //     { depth: 1.0, count: 220, speed: 0.12 }
 // ];
+
+// slower
 const LAYERS = [
   { depth: 0.25, count: 120, speed: 0.01 },  // was 0.02
   { depth: 0.6,  count: 180, speed: 0.03 },  // was 0.06
